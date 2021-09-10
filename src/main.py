b76d30ed6d26540fb410a134b02b4ba5369d0e69
Xavier Coll <62948758+XcollR@users.noreply.github.com>
2021-09-10 18:35:50 +0200

    Added reactions.
    Added reactions below the images in order to generate new cat images :)

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from functions import *
 
 client = discord.Client()
 
-
 #############################################    MAIN    ####################################################
 
 
@@ -15,20 +14,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  message.content = message.content.lower()
   if message.content.startswith(";category"):
-    await cat_category(client, message)
+    await cat_category(message)
 
   elif message.content.startswith(";i"):
     await message.channel.send(welcome_text)
 
   elif message.content.startswith(";cat"):
-    await get_cat(client, message)
+    await get_cat(message)
 
   elif message.content.startswith(";gif"):
-    await get_cat_gif(client, message)
+    await get_cat_gif(message)
+
+@client.event
+async def on_reaction_add(reaction, user):
+  await manage_reactions(reaction, user, client)
+    
 
     
 keep_alive()
 client.run(os.environ['TOKEN'])
-
 
